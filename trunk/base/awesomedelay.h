c234538939611db4ec8a17b4bbe3f698ec04fb39
jasonwynja <jasonwynja@7244580a-3f40-11de-b48d-dbaac48c8fd3>
2009-05-25 02:27:16 +0000

    Added awesomedelay and timer. Cleaned up main.

---
+++ trunk/base/awesomedelay.h
@@ -0,0 +1,11 @@
+/** Delay for around 1 second on an 8 MHz CPU
+ */
+void _delay_1s(void);
+
+/** Delay for around 0.5 seconds on an 8 MHz CPU
+ */
+void _delay_500ms(void);
+
+/** Delay for 160 milliseconds on an 8 MHz CPU
+ */
+void _delay_160ms(void);
