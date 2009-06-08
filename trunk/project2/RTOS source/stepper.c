7720c3ff4ca43c57750344982efbab832c78a0e1
kevlarx <kevlarx@7244580a-3f40-11de-b48d-dbaac48c8fd3>
2009-06-08 04:48:05 +0000

    - Added project 2 source from thumbdrive

---
+++ trunk/project2/RTOS source/stepper.c
@@ -0,0 +1,47 @@
+/** @author Scott Craig
+ *
+ *  @brief Stepper motor sample application.
+ */
+#include <avr/io.h>
+#include "OS/os.h"
+#include "stepper.h"
+
+const unsigned char PPP[] = {1, 2};
+const unsigned int PT = 1;
+
+const uint8_t step_bits[] =
+{
+    STEPPER_RED |   STEPPER_WHITE,
+                    STEPPER_WHITE,
+    STEPPER_BLUE |  STEPPER_WHITE,
+    STEPPER_BLUE,
+    STEPPER_BLUE |  STEPPER_YELLOW,
+                    STEPPER_YELLOW,
+    STEPPER_RED |   STEPPER_YELLOW,
+    STEPPER_RED
+};
+
+void step (void)
+{
+    static uint8_t step_index = 0;
+
+    STEPPER_PORT_DDR |= STEPPER_MASK;
+
+	for(;;)
+	{
+        STEPPER_PORT = (STEPPER_PORT & (uint8_t)(~STEPPER_MASK)) | step_bits[step_index];    
+		
+        Task_Next();
+
+        step_index += STEP;
+        if(step_index >= 8) step_index = 0;
+	}
+}
+
+
+int main(void)
+{
+	Task_Create(step, 1, PERIODIC, 1);
+
+    return 0;
+}
