4258d9d2a3faa536361d24ba0f8af506d13e61c6
kevlarx <kevlarx@7244580a-3f40-11de-b48d-dbaac48c8fd3>
2009-06-09 03:01:05 +0000

    - Added RTOS code, working minimal example

---
+++ trunk/RTOS/main.c
@@ -0,0 +1,29 @@
+#include <LUFA/Drivers/Board/LEDs.h>
+#include <avr/io.h>
+#include "OS/os.h"
+#include "main.h"
+
+const unsigned char PPP[] = {1, 100};
+const unsigned int PT = 1;
+
+void some_task (void)
+{
+	uint8_t LEDMask = LEDS_NO_LEDS;
+
+	while (1) {
+		LEDMask ^= LEDS_LED1;
+		LEDs_SetAllLEDs(LEDMask);
+
+        Task_Next();
+	}
+}
+
+
+int main(void)
+{
+	LEDs_Init();
+
+	Task_Create(some_task, 1, PERIODIC, 1);
+
+    return 0;
+}
