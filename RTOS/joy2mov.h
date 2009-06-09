577a5653670bfb61de69c294e66a74e03c2469b1
kevlarx <kevlarx@7244580a-3f40-11de-b48d-dbaac48c8fd3>
2009-06-09 05:44:51 +0000

    - Working RTOS. Gamepad working. OMG.

---
+++ RTOS/joy2mov.h
@@ -0,0 +1,10 @@
+#ifndef JOYSTICK_TO_MOVEMENT_H_
+#define JOYSTICK_TO_MOVEMENT_H_
+
+#include <stdint.h>
+
+// map X and Y values from a joystick to roomba velocity and radius.
+// uses some lookup tables, and depends on previous calls to do some smoothing
+void joystick_to_movement(uint8_t x, uint8_t y, int16_t* velocity, int16_t* radius);
+    
+#endif
