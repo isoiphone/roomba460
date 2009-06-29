fa030782c84f9de5d7d59042afd72461f4fef840
jasonwynja <jasonwynja@7244580a-3f40-11de-b48d-dbaac48c8fd3>
2009-06-29 01:01:37 +0000

    Committing new and modified files from the thumb drive.

--- trunk/project2/RTOS source/test006_eventwait.c
+++ trunk/project2/RTOS source/test006_eventwait.c
@@ -2,9 +2,9 @@
  * @file   test006.c
  * @author Scott Craig and Justin Tanner
  * @date   Mon Oct 29 16:19:32 2007
- * 
+ *
  * @brief  Test 006 - can the RTOS wait on an event
- * 
+ *
  */
 
 #include "common.h"
@@ -13,21 +13,21 @@
 #include "trace/trace.h"
 
 enum { A=1, B, C, D, E, F, G };
-const unsigned int PT = 3;
 const unsigned char PPP[] = {IDLE, 10, A, 1};
+const unsigned int PT = sizeof(PPP);
 
 extern uint16_t trace_counter;
 EVENT* print_event;
-EVENT* test_event;    
+EVENT* test_event;
 
 void wait_task(void)
 {
     for(;;)
     {
         add_to_trace(1);
-        
+
         Event_Wait(test_event);
-        
+
         add_to_trace(2);
 
         Task_Next();
@@ -39,7 +39,7 @@ void A_task(void)
     for(;;)
     {
         add_to_trace(3);
-        
+
         Signal_And_Next(test_event);
     }
 }
@@ -53,11 +53,11 @@ int main(void)
 
     /* setup the event */
     test_event = Event_Init();
-    print_event = Event_Init();    
-      
+    print_event = Event_Init();
+
    	Task_Create(wait_task, 0, RR, 0);
    	Task_Create(A_task, A, PERIODIC, A);
-	
+
     Event_Wait(print_event);
     print_trace();
 }
