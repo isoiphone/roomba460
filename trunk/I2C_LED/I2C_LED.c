55a456dd24e564d6fbd8a4d40ba6b6aad21a7f0f
kevlarx <kevlarx@7244580a-3f40-11de-b48d-dbaac48c8fd3>
2009-08-13 04:17:16 +0000

    - Interupt driven EYE TWO SEEE codes. Working if you send to global address. Does not seem to work if you send to a specific address (ie: 0x09)

--- trunk/I2C_LED/I2C_LED.c
+++ trunk/I2C_LED/I2C_LED.c
@@ -1,24 +1,23 @@
 #include "global.h"
+#include "i2c.h"
 
-#include "i2csw.h"
-
-#include <util/twi.h>
-#include <util/delay.h>
 #include <avr/interrupt.h>
+#include <util/delay.h>
 
 #define     clock8MHz()    CLKPR = _BV(CLKPCE); CLKPR = 0x00;
 
+
 int main( void )
 {
-	BYTE addr = 0x09;
-	BYTE msg[] = {0xFF, 0xFF, 0xFF}; 
-
 	clock8MHz();
 
 	i2cInit();
 
+	unsigned char cmd[] = {'n', 0xCC, 0x00, 0xFF};
+	unsigned char cmdLen = 4;
+
 	while (1) {
-		i2cSend(addr, 'n', 3, msg); 
+		i2cMasterSend(0x00, cmdLen, cmd);
 		_delay_ms(10);
 	}
 }
